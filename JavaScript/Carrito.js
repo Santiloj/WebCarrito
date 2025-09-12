2762b3f012e12d0551607f0eae28f784a14aa865
Santiago Lopez <santiagolopezjaramillo2005@gmail.com>
2025-09-12 18:26:33 -0500

    Se realizo el java y el carrito

--- JavaScript/Carrito.js
+++ JavaScript/Carrito.js
@@ -1,1 +1,32 @@
+const listProductos = document.getElementById('listProductos');
 
+let productsArray = [];
+
+document.addEventListener('DOMContentLoaded', function()  {
+    eventListeners();
+});
+
+function eventListeners() {
+    listProductos.addEventListener('click', getDataElements);
+}
+
+function getDataElements(e) {
+    if(e.target.classList.contains('btn-add')) {
+       const elementHTML = e.target.parentElement.parentElement;
+       selectData(elementHTML);
+    }
+}  
+
+function selectData(prod) {
+    const productObj = {
+        img: prod.querySelector('img').src,
+        title: prod.querySelector('h4').textContent,
+        precio: parseFloat(prod.querySelector('#currentprice').textContent.replace('$', '')),
+        id: parseInt(prod.querySelector('button[type="button"]').dataset.id, 10),
+        cantidad: 1
+    }
+
+    productsArray = [...productsArray, productObj];
+
+    productsHtml();
+}
