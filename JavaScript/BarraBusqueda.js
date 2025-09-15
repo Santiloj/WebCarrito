7035967a6751ceb7c09371497527673f95a7085e
Santiago Lopez <santiagolopezjaramillo2005@gmail.com>
2025-09-15 15:50:47 -0500

    Se hizo el footer, la barra de busqueda, se implemento la paginacion y se reorganizo el estilo de la pagina

---
+++ JavaScript/BarraBusqueda.js
@@ -0,0 +1,14 @@
+document.addEventListener("keyup", e => {
+
+    if(e.target.matches("#buscador")) {
+       
+        if(e.key === "Escape") e.target.value = ""
+
+        document.querySelectorAll(".producto").forEach(producto => {
+
+            producto.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+            ?producto.classList.remove("filtro")
+            :producto.classList.add("filtro")
+        })
+    }
+})
